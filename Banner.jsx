a7ee8108254e68bfcedd3ac4e9050455f2800c1b
anika557 <ummehabiba19758@gmail.com>
2025-05-09 22:11:09 +0600

    secound commit

--- Banner.jsx
+++ Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
   <div class="hero-content flex-col lg:flex-row">
     <img src="https://placeimg.com/260/400/tech" class="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 class="text-5xl font-bold">Welcome to My Website</h1>
+      <h1 class="text-5xl font-bold">Welcome to My React Website</h1>
       <p class="py-6">This is a simple banner using DaisyUI hero component.</p>
       <button class="btn btn-primary">Get Started</button>
     </div>
